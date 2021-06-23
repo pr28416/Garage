2aff6fae04ae70e3102f0532e3a8ce1cdcf938df
Pranav Ramesh <pranav.ramesh1@gmail.com>
2021-06-23 08:00:16 -0400

    Modified temp

--- temp.py
+++ temp.py
@@ -12,10 +12,17 @@ def binarySearch(x, lst):
     lo, up = 0, len(lst)
     while lo < up:
         mid = (lo+up)//2
-        if x == mid: return mid
-        elif x < mid: up = mid
+        if x == lst[mid]: return mid
+        elif x < lst[mid]: up = mid
         else: lo = mid + 1
 
 
-
-print()
+lst = [1, 3, 5, 7, 9, 11]
+# print(binarySearch(1, lst))
+# print(binarySearch(3, lst))
+# print(binarySearch(5, lst))
+# print(binarySearch(7, lst))
+# print(binarySearch(9, lst))
+# print(binarySearch(11, lst))
+# print(binarySearch(0, lst))
+# print(binarySearch(12, lst))
