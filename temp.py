065d697cc4b9ece1527b6f9660ea5a07d3f1d208
Pranav Ramesh <pranav.ramesh1@gmail.com>
2021-06-24 08:43:03 -0400

    prefix sums

--- temp.py
+++ temp.py
@@ -17,7 +17,25 @@ def binarySearch(x, lst):
         else: lo = mid + 1
 
 
-lst = [1, 3, 5, 7, 9, 11]
+def rangeSums(lst, queries):
+    # Generate prefix sum array - Add an extra 0 at the
+    # front to avoid out-of-bound index errors
+    sums = [0] * (len(lst)+1)
+    for i in range(len(lst)):
+        sums[i+1] = sums[i] + lst[i]
+    # Solve each query, return list of answers
+    answers = [0] * len(queries)
+    for q, query in enumerate(queries):
+        answers[q] = sums[query[1]+1] - sums[query[0]]
+    return answers
+
+# Test code
+rs = rangeSums([2, 11, 5, 3, 6, 8], [(2, 3), (0, 4), (3, 5)])
+for ans in rs: print(ans)
+
+
+
+# lst = [1, 3, 5, 7, 9, 11]
 # print(binarySearch(1, lst))
 # print(binarySearch(3, lst))
 # print(binarySearch(5, lst))
