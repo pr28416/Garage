c5b3f2eca0b3a88d480c2c61fec241d59f140903
Pranav Ramesh <47484624+pr28416@users.noreply.github.com>
2021-06-25 10:41:57 -0400

    Update YouTubeViewFarm.py

--- YouTubeViewFarm.py
+++ YouTubeViewFarm.py
@@ -3,8 +3,6 @@ from time import sleep
 import threading
 from random import randint
 
-# browsers = [webdriver.Chrome("/Users/pranavramesh/Developer/chromedriver") for _ in range(10)]
-# browser = webdriver.Chrome("/Users/pranavramesh/Developer/chromedriver")
 videos = [
     "https://youtu.be/2hmt-nFnKEg",
     "https://youtu.be/mYBBaVnCthc",
@@ -23,45 +21,5 @@ def runVideo(_thread_name):
 
 i = 0
 while i < 100000:
-# for i in range(1000):
     runVideo(f"T{i}")
     i += 1
-# threads = [threading.Thread(target=runVideo, args=(f"T{i}", None)) for i in range(5)]
-# for thd in threads:
-#     thd.start()
-# for thd in threads:
-#     thd.join()
-# counts = [0] * 10
-# while set(counts) != {10}:
-#
-"""
-def print_squares(thread_name, numbers):
-    for number in numbers:
-        print(thread_name, number ** 2)
-
-        # Produce some delay to see the output
-        sleep(1)
-
-
-# Creating 3 threads that execute the same
-# function with different parameters
-thread1 = threading.Thread(target=print_squares,
-                           args=("thread1", [1, 2, 3, 4, 5]))
-
-thread2 = threading.Thread(target=print_squares,
-                           args=("thread2", [6, 7, 8, 9, 10]))
-
-thread3 = threading.Thread(target=print_squares,
-                           args=("thread3", [11, 12, 13, 14, 15]))
-
-# Start the threads
-thread1.start()
-thread2.start()
-thread3.start()
-
-# Join the threads before
-# moving further
-thread1.join()
-thread2.join()
-thread3.join()
-"""
